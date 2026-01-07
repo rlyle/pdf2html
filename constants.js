192218296684dd67e07748a220436ea4a25b637a
Richard Lyle <richard@mancomm.com>
2026-01-07 16:52:29 +0700

    Updated to put the tmp files into the temp directory so this package can work inside a lambda which doesn't allow file writes into the node_modules directory.

--- constants.js
+++ constants.js
@@ -1,12 +1,13 @@
 const path = require('path');
+const os = require('os');
 
 module.exports = {
     VENDOR_PDF_BOX_JAR: 'pdfbox-app-2.0.34.jar',
     VENDOR_TIKA_JAR: 'tika-app-3.2.0.jar',
 
     DIRECTORY: {
-        PDF: path.join(__dirname, './files/pdf/'),
-        IMAGE: path.join(__dirname, './files/image/'),
+        PDF: path.join(os.tmpdir(), './files/pdf/'),
+        IMAGE: path.join(os.tmpdir(), './files/image/'),
         VENDOR: path.join(__dirname, './vendor/'),
     },
 };
